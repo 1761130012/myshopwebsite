f74ebcbc174c4afbc28aeef93fb1dd38031f859b
2890751418 <2890751418@qq.com>
2020-12-24 09:39:30 +0800

    赵明

--- maven-custom-common/src/main/java/com/example/vo/PurchaseShopVo.java
+++ maven-custom-common/src/main/java/com/example/vo/PurchaseShopVo.java
@@ -1,11 +1,14 @@
 package com.example.vo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -21,7 +24,7 @@ import java.util.Date;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@TableName("purchase_shop")
+@TableName(value = "purchase_shop", resultMap = "purchaseShopMap")
 public class PurchaseShopVo implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -65,7 +68,12 @@ public class PurchaseShopVo implements Serializable {
     /**
      * 生产日期
      */
+
+
     @TableField("create_time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    @JSONField(format = "yyyy-MM-dd hh:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     /**
@@ -74,5 +82,15 @@ public class PurchaseShopVo implements Serializable {
     @TableField("save_time")
     private Integer saveTime;
 
+    /*
+     *供货商
+     */
+    @TableField(exist = false)
+    private SupplierVo supplierVo;
 
+    /**
+     * 商品
+     */
+    @TableField(exist = false)
+    private GoodsVo goodsVo;
 }
--- maven-custom-web/src/main/java/com/example/controller/PurchaseController.java
+++ maven-custom-web/src/main/java/com/example/controller/PurchaseController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.service.PurchaseService;
+import com.example.vo.GoodsVo;
+import com.example.vo.PurchaseShopVo;
 import com.example.vo.PurchaseVo;
 import com.example.vo.WarehouseVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,12 +28,33 @@ public class PurchaseController {
     @Autowired
     PurchaseService purchaseService;
 
+    @RequestMapping("queryshenpiPageVo")
+    public Page<PurchaseVo> queryshenpiPageVo(@RequestParam(value = "page", defaultValue = "1") int page,
+                                              @RequestParam(value = "rows", defaultValue = "5") int rows,
+                                              PurchaseVo purchaseVo) {
+        purchaseVo.setState(0);
+        return purchaseService.selectPageVo(new Page<PurchaseVo>(page, rows), purchaseVo);
+    }
+
+    //采购单信息
     @RequestMapping("queryPageVo")
     public Page<PurchaseVo> queryPageVo(@RequestParam(value = "page", defaultValue = "1") int page,
                                         @RequestParam(value = "rows", defaultValue = "5") int rows,
                                         PurchaseVo purchaseVo) {
 
         return purchaseService.selectPageVo(new Page<PurchaseVo>(page, rows), purchaseVo);
+    }
+
+    //采购单详情信息查询
+    @RequestMapping("selectShopPageVo")
+    public Page<PurchaseShopVo> selectShopPageVo(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                 @RequestParam(value = "rows", defaultValue = "5") int rows,
+                                                 PurchaseShopVo purchaseShopVo, String name) {
+
+        GoodsVo goodsVo = new GoodsVo();
+        goodsVo.setName(name);
+        purchaseShopVo.setGoodsVo(goodsVo);
+        return purchaseService.selectShopPageVo(new Page<PurchaseShopVo>(page, rows), purchaseShopVo);
     }
 
 }
